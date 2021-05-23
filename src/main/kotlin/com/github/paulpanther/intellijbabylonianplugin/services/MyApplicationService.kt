e42e3f95af980a9231c092d1bda29e24ba35a476
GitHub Action <action@github.com>
2021-05-23 19:00:54 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/paulpanther/intellijbabylonianplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.paulpanther.intellijbabylonianplugin.services
+
+import com.github.paulpanther.intellijbabylonianplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
